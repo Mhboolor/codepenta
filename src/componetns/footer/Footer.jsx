2426de25a33a746c4d1e4b0ba9a5bf1ae28fe825
Mhboolor <mhstare81@gmail.com>
2023-09-13 19:56:45 +0330

    fix : fix copyright responsive

--- src/componetns/footer/Footer.jsx
+++ src/componetns/footer/Footer.jsx
@@ -4,7 +4,7 @@ import Navbar from "./content/Navbar";
 function Footer() {
   return (
     <footer className="shadow-[0px_-10px_25px_-10px_rgba(0,0,0,0.1)] mt-20">
-      <div className="flex items-center justify-evenly container m-auto px-5 sm:px-0 py-8">
+      <div className="flex flex-col-reverse items-center justify-evenly container m-auto px-5 sm:px-0 py-8 gap-5 md:flex-row">
         <Navbar />
         <CopyRight />
       </div>
