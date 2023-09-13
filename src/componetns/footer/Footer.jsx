7cfb2892cd466bab101d7faa8185fb443318f1ba
Mhboolor <mhstare81@gmail.com>
2023-09-13 11:09:28 +0330

    feat : add shadow for footer

--- src/componetns/footer/Footer.jsx
+++ src/componetns/footer/Footer.jsx
@@ -1,15 +1,15 @@
-import CopyRight from "./content/CopyRight"
-import Navbar from "./content/Navbar"
+import CopyRight from "./content/CopyRight";
+import Navbar from "./content/Navbar";
 
 function Footer() {
   return (
-    <footer className="shadow-2xl">
-      <div className="flex items-center justify-evenly container m-auto px-5 sm:px-0 py-6">
-        <Navbar/>
-        <CopyRight/>
+    <footer className="shadow-[0px_-10px_25px_-10px_rgba(0,0,0,0.1)]">
+      <div className="flex items-center justify-evenly container m-auto px-5 sm:px-0 py-8">
+        <Navbar />
+        <CopyRight />
       </div>
     </footer>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
